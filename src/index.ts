dd5058ac501e3554b9f34e58ab62abff95e93168
Khaif Mohamed <khaifmohamededava@gmail.com>
2024-01-06 15:25:56 +0530

    change in urls

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ app.use(
     origin:
       process.env.NODE_ENV === "development"
         ? "http://localhost:5173"
-        : "mern-gpt.vercel.app",
+        : "https://mern-gpt.vercel.app",
 
     credentials: true,
   })
